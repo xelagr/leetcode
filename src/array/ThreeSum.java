7918b7686de2d71716f32f401b5dbc395689e8b6
alex <123>
2020-02-09 23:34:14 -0500

    Three Sum

---
+++ src/array/ThreeSum.java
@@ -0,0 +1,57 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ThreeSum {
+
+    // complexity O(N^2)
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> triples = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (k < nums.length-1 && nums[k] == nums[k+1]) {
+                    k--;
+                    continue;
+                }
+                if (nums[i] + nums[j] + nums[k] > 0) {
+                    k--;
+                }
+                else if (nums[i] + nums[j] + nums[k] < 0) {
+                    j++;
+                }
+                else {
+                    triples.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                }
+            }
+        }
+        return triples;
+    }
+
+
+    // complexity O(N^2 * logN)
+    public List<List<Integer>> threeSum2(int[] nums) {
+        Set<List<Integer>> triples = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int k = Arrays.binarySearch(nums, j + 1, nums.length, -(nums[i] + nums[j]));
+                if (k >= 0) {
+                    triples.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                }
+            }
+        }
+        return new ArrayList<>(triples);
+    }
+}
