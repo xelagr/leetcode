e1a7d73617f6a2a835b085cc085101f67c97b300
agrishkov <agrishkov@cppib.com>
2020-02-11 08:45:35 -0500

    Move Zeroes improvement

--- src/array/MoveZeroes.java
+++ src/array/MoveZeroes.java
@@ -3,6 +3,20 @@ package array;
 public class MoveZeroes {
     public void moveZeroes(int[] nums) {
         int swaps = 0;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for (int i = j; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+        System.out.println(swaps);
+    }
+
+    public void moveZeroes2(int[] nums) {
+        int swaps = 0;
         int end = nums.length;
         for (int i = nums.length-1; i >= 0; i--) {
             if (nums[i] == 0) {
