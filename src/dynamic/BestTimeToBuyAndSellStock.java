83599e34396a664313b07d4c62bb32434db4a221
agrishkov <agrishkov@cppib.com>
2020-02-20 15:42:37 -0500

    Best Time to Buy and Sell Stock

--- src/dynamic/BestTimeToBuyAndSellStock.java
+++ src/dynamic/BestTimeToBuyAndSellStock.java
@@ -2,18 +2,15 @@ package dynamic;
 
 public class BestTimeToBuyAndSellStock {
     public int maxProfit(int[] prices) {
-        int buy = Integer.MAX_VALUE, sell = 0, profit = 0;
+        int minPrice = Integer.MAX_VALUE, maxProfit = 0;
         for (int price : prices) {
-            if (price < buy) {
-                buy = sell = price;
+            if (price < minPrice) {
+                minPrice = price;
             }
-            if (price > sell) {
-                sell = price;
-            }
-            if (sell - buy > profit) {
-                profit = sell - buy;
+            else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
             }
         }
-        return profit;
+        return maxProfit;
     }
 }
