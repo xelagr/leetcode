a561a4bded5e96f9f9037baf4bea6f91d3b32b15
agrishkov <agrishkov@cppib.com>
2020-03-02 12:30:32 -0500

    Binary Tree Level Order Traversal - a slight optimization

--- src/tree/BinaryTreeLevelOrderTraversal.java
+++ src/tree/BinaryTreeLevelOrderTraversal.java
@@ -9,23 +9,20 @@ public class BinaryTreeLevelOrderTraversal {
         List<List<Integer>> nodeValues = new ArrayList<>();
         Queue<TreeNode> queue = new ArrayDeque<>();
         queue.add(root);
-        int levelWidth = 1;
+        int levelWidth;
         while(!queue.isEmpty()) {
             List<Integer> levelNodes = new ArrayList<>();
-            int nextLevelWidth = 0;
+            levelWidth = queue.size();
             for (int i = 0; i < levelWidth; i++) {
                 TreeNode node = queue.poll();
                 levelNodes.add(node.val);
                 if (node.left != null) {
                     queue.add(node.left);
-                    nextLevelWidth++;
                 }
                 if (node.right != null) {
                     queue.add(node.right);
-                    nextLevelWidth++;
                 }
             }
-            levelWidth = nextLevelWidth;
             nodeValues.add(levelNodes);
         }
         return nodeValues;
